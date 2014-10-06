40a77f797a53d1cc368d0303d7c5f5db6cb3ff17
Andy Roddam <andybendyman@hotmail.com>
2014-10-06 08:57:59 +0100

    updated

---
+++ samples/Authentication_01/create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('email',50)->unique();
+            $table->string('username',20);
+            $table->string('password',60);
+            $table->string('password_temp',60);
+            $table->string('code',60);
+            $table->tinyInteger('active');
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
