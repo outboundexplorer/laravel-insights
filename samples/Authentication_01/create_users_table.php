192ca32b4d66974e3a875267fea958e9387e7341
Andy Roddam <andybendyman@hotmail.com>
2014-10-06 08:59:19 +0100

    Update create_users_table.php

--- samples/Authentication_01/create_users_table.php
+++ samples/Authentication_01/create_users_table.php
@@ -1,4 +1,5 @@
 <?php
+// app/database/migrations/create_users_table.php
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -15,13 +16,13 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-            $table->string('email',50)->unique();
-            $table->string('username',20);
-            $table->string('password',60);
-            $table->string('password_temp',60);
-            $table->string('code',60);
-            $table->tinyInteger('active');
-            $table->timestamps();
+            		$table->string('email',50)->unique();
+            		$table->string('username',20);
+            		$table->string('password',60);
+            		$table->string('password_temp',60);
+            		$table->string('code',60);
+            		$table->tinyInteger('active');
+            		$table->timestamps();
 		});
 	}
 
