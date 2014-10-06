6ff04b5604f69b95ff9f2aa5558d8741d70c7ff7
Andy Roddam <andybendyman@hotmail.com>
2014-10-06 08:45:27 +0100

    Update home.blade.php

--- samples/Authentication_01/views/home.blade.php
+++ samples/Authentication_01/views/home.blade.php
@@ -1,19 +1,12 @@
 @extends ('layouts.main')
-{{--
-Access the main.blade.php template
->>> GOTO main.blade.php
---}}
-
-
 
 @section ('content')
 
     @if(Auth::check())
-        <p>Hello, {{ Auth::user()->username }}</p>  <!-- Auth::user() is current user instance -->
+        <p>Hello, {{ Auth::user()->username }}</p>  <!-- Auth::user() is current user -->
     @else
         <p>You are not signed in</p>
     @endif
     This is the new homepage.
 @stop
 
-<!-- >>> GOTO routes.php>'account/create (GET)'
